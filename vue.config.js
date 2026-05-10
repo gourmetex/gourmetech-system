35b43df24894621865881a849cefd0dd68ccbc65
Saymon <linnubr@gmail.com>
2026-05-09 22:44:31 -0300

    correção do server de desenvolvimento

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
         target: 'https://viacep.com.br',
